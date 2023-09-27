58a89ebef2ef3090e9d394d5e8d2b337443b1563
nag92 <nag92@me.com>
2023-09-26 20:40:24 -0400

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/labs/catkin_ws/src
+# Build directory: /workspaces/labs/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
